0570ab27c95c30260e2c81567b74f2ab27fbd972
nilpa018 <nilpa018@yahoo.fr>
2024-04-22 13:43:39 +0200

    First commit to init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Open Street Map - Select points</h1>
+        <p>Permet l'affichage d'une map contenant des géopoints et une façon de séléctionner plusieurs de ces points.</p>
+    </>
+  )
+}
+
+export default App
